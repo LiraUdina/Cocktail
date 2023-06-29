21c303497b285da416a04a648c1631bc5d0cb0d9
Lira <Lirawww@mail.ru>
2023-06-29 14:04:04 +0500

    Поиск коктейлей

--- script.js
+++ script.js
@@ -1,89 +1,93 @@
-const nameInput = document.getElementById('namecock');
-const resetButton = document.getElementById('throwoff');
-let content = {
-  title: document.getElementById('coctailTitle'),
-  description: document.getElementById('coctailDescription'),
-  list: document.getElementById('coctailIngredients')
-};
+const nameInput = document.getElementById('namecock'); // Получаем элемент ввода имени
+const resetButton = document.getElementById('throwoff'); // Получаем кнопку сброса
+const contentElement = document.getElementById('content'); // Получаем элемент контента
 
-const contentBlock = document.getElementsByClassName('content-block')[0];
+function displayCocktails(data) {
+  contentElement.innerHTML = ''; 
 
-const savedValue = localStorage.getItem('name');
-nameInput.value = savedValue;
+  if (data.drinks !== null) {
+    for (let kol = 0; kol < data.drinks.length; kol++) {
+      const cocktail = data.drinks[kol];
+    // Определяем каждый коктейль в массиве drinks и сохраняем его в переменную cocktail
+    // Далее можно использовать переменную cocktail для получения данных о конкретном коктейле
+ 
+      const title = cocktail.strDrink; 
+      const description = cocktail.strInstructions; 
+      const imageUrl = cocktail.strDrinkThumb; 
+
+      const ingredients = [];
+      const measures = [];
+
+      for (let i = 1; i <= 15; i++) {
+        const ingredient = cocktail[`strIngredient${i}`];
+        const measure = cocktail[`strMeasure${i}`];
+
+        if (ingredient !== null) {
+          ingredients.push(ingredient);
+          measures.push(measure);
+        }
+      }
+
+      const contentBlock = document.createElement('div');
+      contentBlock.classList.add('content-block'); // Добавляем класс блока контента
+
+      const imgElement = document.createElement('img');
+      imgElement.src = imageUrl;
+      imgElement.classList.add('coctail-image'); // Добавляем класс изображения коктейля
+      contentBlock.appendChild(imgElement);
+
+      const titleElement = document.createElement('h2');
+      titleElement.innerHTML = title;
+      titleElement.classList.add('coctail'); // Добавляем класс заголовка коктейля
+      contentBlock.appendChild(titleElement);
+
+      const descriptionElement = document.createElement('p');
+      descriptionElement.innerHTML = description;
+      descriptionElement.classList.add('coctail2'); // Добавляем класс описания коктейля
+      contentBlock.appendChild(descriptionElement);
+
+      const ingredientsListElement = document.createElement('ul');
+      ingredients.forEach((ingredient) => {
+        const li = document.createElement('li');
+        li.innerHTML = ingredient;
+        ingredientsListElement.appendChild(li);
+      });
+      ingredientsListElement.classList.add('coctail-ingr'); // Добавляем класс списка ингредиентов коктейля
+      contentBlock.appendChild(ingredientsListElement);
+
+      contentElement.appendChild(contentBlock); // Добавляем блок контента в элемент контента
+    }
+  }
+}
 
 function fetchCocktails(inputText) {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputText}`)
+    .then((response) => response.json())
     .then((data) => {
-      console.log(data)
-      return data.json();
-
-    })
-    .then((data) => {
-  
-      if (data.drinks !== null) {
-        let content = document.getElementById('content');
-        content.innerHTML = '';
-
-        for (let kol = 0; kol < data.drinks.length; kol++) {
-          const cocktail = data.drinks[kol];
-
-          const title = cocktail.strDrink;
-          const description = cocktail.strInstructions;
-          const imageUrl = cocktail.strDrinkThumb;
-
-          const ingredients = [];
-          const measures = [];
-
-          for (let i = 1; i <= 15; i++) {
-            const ingredient = cocktail[`strIngredient${i}`];
-            const measure = cocktail[`strMeasure${i}`];
-
-            if (ingredient !== null) {
-              ingredients.push(ingredient);
-              measures.push(measure);
-            }
-          }
-
-          const contentElement = document.getElementById('content');
-          contentElement.innerHTML = '';
-          
-          const contentBlock = document.createElement('div');
-          contentBlock.classList.add('content-block');
-          
-          const imgElement = document.createElement('img');
-          imgElement.src = imageUrl;
-          imgElement.classList.add('coctail-image');
-          contentBlock.appendChild(imgElement);
-          
-          const titleElement = document.createElement('h2');
-          titleElement.innerHTML = title;
-          titleElement.classList.add('coctail');
-          contentBlock.appendChild(titleElement);
-          
-          const descriptionElement = document.createElement('p');
-          descriptionElement.innerHTML = description;
-          descriptionElement.classList.add('coctail2');
-          contentBlock.appendChild(descriptionElement);
-          
-          const ingredientsListElement = document.createElement('ul');
-          ingredients.forEach((ingredient) => {
-            const li = document.createElement('li');
-            li.innerHTML = ingredient;
-            ingredientsListElement.appendChild(li);
-          });
-          ingredientsListElement.classList.add('coctail-ingr');
-          contentBlock.appendChild(ingredientsListElement);
-          
-          contentElement.appendChild(contentBlock);
-        }
-      }
-      return data
+      displayCocktails(data);
+      localStorage.setItem('name', encodeURIComponent(inputText)); // Сохраняем значение в локальном хранилище
     });
 }
 
 resetButton.addEventListener('click', () => {
   nameInput.value = '';
-  content.title.innerHTML = '';
-  content.description.innerHTML = '';
-  content.list.innerHTML = '';
+  contentElement.innerHTML = '';
+  localStorage.removeItem('name'); // Удаляем значение из локального хранилища
 });
+
+nameInput.addEventListener('change', () => {
+  const inputText = nameInput.value.trim();
+  if (inputText !== '') {
+    fetchCocktails(inputText); // Вызов функции для загрузки коктейлей по введенному тексту
+  }
+});
+
+window.addEventListener('load', () => {
+  const savedValue = localStorage.getItem('name');
+  if (savedValue) {
+    nameInput.value = decodeURIComponent(savedValue); 
+    fetchCocktails(savedValue); // Вызов функции для загрузки коктейлей по сохраненному значению
+  }
+});
+
+fetchCocktails('');
